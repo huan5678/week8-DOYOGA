f93850056e2e217e5dd72cd53b997ddb3fcf9f5d
huan5678 <huan5678@gmail.com>
2021-08-24 02:53:59 +0800

    reservation

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,3 +5,30 @@ const swiper = new Swiper(".desktopSwiper", {
     hide: true,
   },
 });
+
+const customSwiper = new Swiper(".customSwiper",{
+  slidesPerView: .98,
+  slidesPerColumn: 3,
+  spaceBetween: 8,
+  slidesPerColumnFill: 'row',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    768: {
+      slidesPerView: 2.1,
+      slidesPerColumn: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30,
+    }
+  }
+})
+
+$("button.navbar-toggler").click(function(){
+  $(this).children().toggleClass("navbar-toggler-icon-close");
+})
